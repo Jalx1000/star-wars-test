e0413582ee9144c5c6166d51747d040833dc273b
JAL <jalejandromogro1@gmail.com>
2022-08-26 17:51:46 -0400

    logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,92 @@
+import { useState, useEffect, useRef } from "react";
+import { getCharacter, getPeople, searchCharacter } from "./api/people";
 import "./App.css";
-import data from './data.json'
 
 function App() {
+  const inputSearch = useRef();
+  const [textSearch, setTextSearch] = useState("");
+  const [people, setPeople] = useState([]);
+  const [currentCharacter, setCurrentCharacter] = useState(1);
+  const [details, setDetails] = useState({});
+  const [page, setPage] = useState(1)
+  const [errorState, setErrorState] = useState({ hasError: false });
+
+  useEffect(() => {
+    getPeople(page).then(setPeople).catch(handleError);
+  }, [page]);
+
+  useEffect(() => {
+    getCharacter(currentCharacter).then(setDetails).catch(handleError);
+  }, [currentCharacter]);
+
+  const handleError = (err) => {
+    setErrorState({ hasError: true, message: err.message });
+  };
+
+  const showDetails = (character) => {
+    const id = Number(character.url.split("/").slice(-2)[0]);
+    setCurrentCharacter(id);
+  };
+
+  const onChangeTextSearch = (e) => {
+    e.preventDefault();
+    const text = inputSearch.current.value;
+    setTextSearch(text);
+  };
+
+  const onSearchSubmit = (e) => {
+    if (e.key !== "Enter") return;
+
+    inputSearch.current.value = "";
+    setDetails({});
+    searchCharacter(textSearch)
+      .then(setPeople)
+      .catch(handleError);
+  };
+
+  const onChangePage = (next)=>{
+    if(!people.previous && page + next <=0 ) return;
+    if(!people.next && page + next>=9) return;
+    setPage(page+next)
+  }
+
   return (
-    <ul>
-      {data.results.map(charater=>{
-        return <li key={charater.name}>{charater.name}</li>
-      })}
-    </ul>
+    <div>
+      <input
+        ref={inputSearch}
+        onChange={onChangeTextSearch}
+        onKeyDown={onSearchSubmit}
+        type="text"
+        placeholder="Buscar un personaje"
+      />
+      <ul>
+        {errorState.hasError && <>{errorState.message}</>}
+        {people?.results?.map((charater) => {
+          return (
+            <li key={charater.name} onClick={() => showDetails(charater)}>
+              {charater.name}
+            </li>
+          );
+        })}
+      </ul>
+
+      <section>
+        <button onClick={()=>onChangePage(-1)}>Prev</button>
+        | {page} |
+        <button onClick={()=>onChangePage(1)}>Next</button>
+      </section>
+
+      {details && (
+        <aside>
+          <h1>{details.name}</h1>
+          <ul>
+            <li>height: {details.height}</li>
+            <li>mass: {details.mass}</li>
+            <li>Year of Birth: {details.birth_year}</li>
+          </ul>
+        </aside>
+      )}
+    </div>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,27 @@
 import { render, screen } from "@testing-library/react";
+import {jest} from '@jest/globals';
 import App from "./App";
 import data from "./data.json";
 
 describe("Star Wars App", () => {
-  it("Should show a list of characters including Luke Skywalker", () => {
+  beforeAll(() => jest.spyOn(window, fetch));
+  
+  it('"Should show a lis of characters from the API', async () => {
+    Window.fetch.mockResolvedValueOnce({
+      ok: true,
+      json: async () => data,
+    });
     render(<App />);
-    expect(screen.getByText("Luke Skywalker")).toBeInTheDocument();
-  });
-});
+    expect(window.fetch).toHaveBeenCalledTimes(1);
+    expect(window.fetch).toHaveBeenCalledWith("https://swapi.dev/api/people/");
 
-describe("Should show a lis of characters from a JSON file", () => {
-  it("Should show a list of characters including Luke Skywalker", () => {
-    render(<App />);
     for (let character of data.results) {
-      expect(screen.getByText(character.name)).toBeInTheDocument();
+      expect(await screen.findByText(character.name)).toBeInTheDocument();
     }
   });
+  it('should show a error message when has a network error',async ()=>{
+    window.fetch.mockResolvedValueOnce(new Error("Network Error"))
+    render(<App/>);
+    expect(await screen.findByText('Network Error')).toBeInTheDocument(); 
+  })
 });
